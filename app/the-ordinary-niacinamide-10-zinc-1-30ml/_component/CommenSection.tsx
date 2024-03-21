a9ac42f699e3c0657aed832159d375f5f5b791fd
anishkarnik <guest2079@git.org.in>
2024-03-21 21:12:42 +0530

    modified:   app/the-ordinary-niacinamide-10-zinc-1-30ml/_component/CommenSection.tsx 	new file:   components/Review.tsx 	new file:   components/ui/card.tsx

--- app/the-ordinary-niacinamide-10-zinc-1-30ml/_component/CommenSection.tsx
+++ app/the-ordinary-niacinamide-10-zinc-1-30ml/_component/CommenSection.tsx
@@ -1,13 +1,17 @@
 'use client'
-import { Card, CardBody, CardFooter, Divider, Progress } from "@nextui-org/react"
+import Review from "@/components/Review"
+import { Button } from "@/components/ui/button"
+import { Card, CardDescription, CardFooter, CardHeader } from "@/components/ui/card"
+import { Progress } from "@nextui-org/react"
 import { User, Link } from "@nextui-org/react"
 import { FaStar } from "react-icons/fa"
 
 export const CommenSection = () => {
   return (
-    <div className='my-20 px-10'>
+    <>
+    <div className='my-20 pt-5 max-w-full px-10 border-t'>
         <h1 className='font-medium text-3xl'><span className='font-semibold text-primary'>| </span>Product Ratings & Reviews</h1>
-        <div className='flex flex-row'>
+        <div className='flex flex-col lg:flex-row gap-x-10'>
             <div className='flex flex-col pl-10 gap-y-5'>
                 <p className='text-2xl font-medium mt-10 '>Overall Rating</p>
                 <div className="flex flex-row items-center space-x-1 gap-x-1">
@@ -16,7 +20,7 @@ export const CommenSection = () => {
                         return <FaStar className="text-yellow-400" key={star} />
                     })}
                 </div>
-                <div className="flex flex-col space-y-2 w-[400px]">
+                <div className="flex flex-col space-y-5 w-[300px]">
                     <div className="flex flex-row items-center space-x-2">
                     <p className="text-lg">5</p>
                     <FaStar className="text-yellow-400" />
@@ -42,9 +46,76 @@ export const CommenSection = () => {
                     <FaStar className="text-yellow-400" />
                     <Progress size="sm" color="success" aria-label="Loading..." value={0} />
                     </div>
+                    <div className="flex mx-auto">
+                        <Review />
+                    </div>
                 </div>
+            </div>
+            <div className="flex flex-col w-full mt-10 gap-y-3">
+                <Card>
+                    <CardHeader>
+                        <div className="flex flex-row">
+                        <User   
+                            name="Jane Doe"
+                            description={(
+                                <Link href="https://twitter.com/jrgarciadev" size="sm" isExternal>
+                                  @jrgarciadev
+                                </Link>
+                              )}
+                            avatarProps={{
+                                src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+                            }}
+                            />
+                        </div>
+                    </CardHeader>
+                    <CardFooter>
+                        <p className="text-muted-foreground pb-2 text-xs">Got this for online meetings during the pandemic--the mic on my laptop is pretty crappy and besides I wanted something that can still pick up my voice clearly from a short distance away. Especially given how much time virtual meetings are </p>     
+                    </CardFooter>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <div className="flex flex-row">
+                        <User   
+                            name="Jane Doe"
+                            description={(
+                                <Link href="https://twitter.com/jrgarciadev" size="sm" isExternal>
+                                  @jrgarciadev
+                                </Link>
+                              )}
+                            avatarProps={{
+                                src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+                            }}
+                            />
+                        </div>
+                    </CardHeader>
+                    <CardFooter>
+                        <p className="text-muted-foreground pb-2 text-xs">Got this for online meetings during the pandemic--the mic on my laptop is pretty crappy and besides I wanted something that can still pick up my voice clearly from a short distance away. Especially given how much time virtual meetings are </p>     
+                    </CardFooter>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <div className="flex flex-row">
+                        <User   
+                            name="Jane Doe"
+                            description={(
+                                <Link href="https://twitter.com/jrgarciadev" size="sm" isExternal>
+                                  @jrgarciadev
+                                </Link>
+                              )}
+                            avatarProps={{
+                                src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+                            }}
+                            />
+                        </div>
+                    </CardHeader>
+                    <CardFooter>
+                        <p className="text-muted-foreground pb-2 text-xs">Got this for online meetings during the pandemic--the mic on my laptop is pretty crappy and besides I wanted something that can still pick up my voice clearly from a short distance away. Especially given how much time virtual meetings are </p>     
+                    </CardFooter>
+                </Card>
+                <Button variant='link'>View more</Button>
             </div>
             </div>
         </div>
+        </>
   )
 }
