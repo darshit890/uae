16a07e4fafe2744a05abdb690f26a264a11b6119
DarshitRajvansh <darshitrajvansh890@gmail.com>
2024-03-26 19:36:16 +0530

    deleted:    sanity/sanity.config.ts 	modified:   schemaTypes/product.ts

--- schemaTypes/product.ts
+++ schemaTypes/product.ts
@@ -71,10 +71,5 @@ export const product = defineType({
             type: "array",
             of: [{ type: "string"}]
         },
-        {
-            title: "Video file",
-            name: "video",
-            type: "mux.video"
-          },
     ],
 })
