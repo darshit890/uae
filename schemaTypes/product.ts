60f1039c8f6031a5b5ae5b8985ecf1a33139765b
DarshitRajvansh <darshitrajvansh890@gmail.com>
2024-03-26 18:33:29 +0530

    modified:   app/sanity.config.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   schemaTypes/product.ts

--- schemaTypes/product.ts
+++ schemaTypes/product.ts
@@ -71,7 +71,10 @@ export const product = defineType({
             type: "array",
             of: [{ type: "string"}]
         },
-        
-
+        {
+            title: "Video file",
+            name: "video",
+            type: "mux.video"
+          },
     ],
 })
