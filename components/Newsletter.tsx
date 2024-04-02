6ea3315a3151702630963c116c4b29b406497bf3
DarshitRajvansh <darshitrajvansh890@gmail.com>
2024-04-03 00:52:50 +0530

    modified:   components/Newsletter.tsx

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -20,6 +20,7 @@ export default function NewsLetter() {
               <ModalHeader className="flex flex-col gap-1">Newsletter</ModalHeader>
               <ModalBody>
                 <Input type="email" label="Email" placeholder="Enter your email" />
+                <p className="text-md text-gray-700 font-medium">Subscribe to the mailing list to recieve updates on new arrivals, special offers and other.</p>
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
