043acc31d6d08a005e9a4500823e9c86db0cbba3
DarshitRajvansh <darshitrajvansh890@gmail.com>
2024-04-03 12:23:55 +0530

    modified:   components/CartSidebar.tsx

--- components/CartSidebar.tsx
+++ components/CartSidebar.tsx
@@ -24,7 +24,7 @@ export function CartSidebar() {
       </SheetTrigger>
       <SheetContent className="sm:max-w-lg w-[90vw]">
         <SheetHeader>
-          <SheetTitle>Wishlist</SheetTitle>
+          <SheetTitle>Cart</SheetTitle>
         </SheetHeader>
         <div className="h-full flex flex-col justify-between">
           <div className="mt-8 flex-1 overflow-y-auto">
